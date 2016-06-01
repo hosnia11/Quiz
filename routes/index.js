5d0c3cf4e499eeddb7faea30fb6a4f1d4155659a
hosnia11 <hosnia-11@hotmail.com>
2016-06-02 01:49:28 +0200

    Gestión de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require ('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.param('quizId', quizController.load);
+router.param('userId', userController.load); 
 
 router.get('/quizes.:format?', quizController.index);
 router.get('/quizes/:quizId(\\d+).:format?', quizController.show);
@@ -21,6 +23,14 @@ router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
+router.get('/users', userController.index);   
+router.get('/users/:userId(\\d+)', userController.show);   
+router.get('/users/new', userController.new);    
+router.post('/users', userController.create);  
+router.get('/users/:userId(\\d+)/edit', userController.edit);     
+router.put('/users/:userId(\\d+)', userController.update);   
+router.delete('/users/:userId(\\d+)', userController.destroy);
 
 
 /* GET author*/
